61712597e9d52b390df935c9a56dcdef22365d6f
dark98 <darkaidz98@gmail.com>
2017-08-08 03:38:03 +0100

    update

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,7 +118,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(VENDOR_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/Android.mk
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wifi
